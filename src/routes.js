55284d6ca62ba2f863e4119f4eabf790695d7b0b
Khaled Sami <k.sami.mohammed@gmail.com>
2017-04-11 23:34:07 +0100

    Add a button to link the new posts page

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,11 @@ import {Route, IndexRoute} from 'react-router';
 
 import App from './components/app';
 import PostIndex from './components/post-index';
+import PostNew from './components/post-new';
 
 export default (
 	<Route path="/" component={App}>
 		<IndexRoute component={PostIndex} />
+		<Route path="posts/new" component={PostNew} />
 	</Route>
 );
