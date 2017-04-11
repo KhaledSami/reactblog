3fdaed0edbff7d42d8a30ad12eb9c9a371de4432
Khaled Sami <k.sami.mohammed@gmail.com>
2017-04-11 23:50:57 +0100

    add the form structure using redux-form

--- src/components/post-new.js
+++ src/components/post-new.js
@@ -1,12 +1,46 @@
-import React, {Component} from 'react';
+import React, { Component, PropTypes } from 'react';
+import { reduxForm } from 'redux-form';
 
-class PostNew extends Component {
-	render() {
-		return(
-			<div>
-			</div>
-		);
-	}
+class PostsNew extends Component {
+  render() {
+    const { fields: { title, categories, content }, handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit}>
+
+        <h3>Create A New Post</h3>
+        
+        <div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`}>
+          <label>Title</label>
+          <input type="text" className="form-control" {...title} />
+          <div className="text-help">
+            {title.touched ? title.error : ''}
+          </div>
+        </div>
+
+        <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' : ''}`}>
+          <label>Categories</label>
+          <input type="text" className="form-control" {...categories} />
+          <div className="text-help">
+            {categories.touched ? categories.error : ''}
+          </div>
+        </div>
+
+        <div className={`form-group ${content.touched && content.invalid ? 'has-danger' : ''}`}>
+          <label>Content</label>
+          <textarea className="form-control" {...content} />
+          <div className="text-help">
+            {content.touched ? content.error : ''}
+          </div>
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+    );
+  }
 }
 
-export default PostNew;
+export default reduxForm({
+  form: 'PostsNewForm',
+  fields: ['title', 'categories', 'content'],
+})(PostsNew);
