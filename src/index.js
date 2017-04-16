6aec81bb4dcaa185d962a5e7915db6976403ddf0
Khaled Sami <k.sami.mohammed@gmail.com>
2017-04-16 22:18:24 +0100

    Add show post page

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
-import reducers from './reducers';
 import { Router, browserHistory } from 'react-router';
+import reducers from './reducers';
 import routes from './routes';
 import promise from 'redux-promise';
 
-const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+const createStoreWithMiddleware = applyMiddleware(
+  promise
+)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router history={browserHistory} routes={routes}/>
+    <Router history={browserHistory} routes={routes} />
   </Provider>
   , document.querySelector('.container'));
